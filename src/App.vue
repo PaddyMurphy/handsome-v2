087b690fa76e43102a267d33c9a762c6969991cf
PaddyMurphy <patrickblewis@gmail.com>
2017-07-12 11:44:26 -0500

    change build path

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 -->
 <template>
   <div id="app" :class="{ open: menuOpen }">
-    <div class="logo" style="background-image: url('static/img/logo-2x.png')"><b class="show-accessible">Southwestern University</b></div>
+    <div class="logo"><b class="show-accessible">Southwestern University</b></div>
 
     <label class="menu-label">
       <button @click="toggleMenu" role="button" aria-label="toggle menu" class="button menu-link">
@@ -48,35 +48,35 @@
         <div class="side-content">
 
           <div class="card">
-            <img src="static/img/side-forbes.png" alt="">
+            <img src="./img/side-forbes.png" alt="">
             <small class="card-subtitle">ABOUT NOLS</small>
             <h2 class="title">FORBES</h2>
             <p class="info">Forbes names NOLS as the #1 outdoor education school in its 2016 Grateful Grad Colleges. NOLS is also recognized as one of Forbes’ top 50 colleges in the South.</p>
           </div>
 
           <div class="card">
-            <img src="static/img/side-jacobswell.png" alt="">
+            <img src="./img/side-jacobswell.png" alt="">
             <small class="card-subtitle">ATHLETICS</small>
             <h2 class="title">Jacob's Well Diving</h2>
             <p class="info">NOLS men's swimming finishes second, women third at SCAC Championships. Mickey Scharbrough captured the 200-yard breaststroke with a time of 2:06.42, while Belaineh also finished third in the 100-yard fr...</p>
           </div>
 
           <div class="card">
-            <img src="static/img/side-mtadams.png" alt="">
+            <img src="./img/side-mtadams.png" alt="">
             <small class="card-subtitle">ACADEMICS</small>
             <h2 class="title">School of Mountaineering</h2>
             <p class="info">The School of Mountaineering at NOLS University offers a world-class arts education to our students, and serves the Central Texas community by offering a wide selection of events, each year, in performance and visual arts.</p>
           </div>
 
           <div class="card">
-            <img src="static/img/side-devilsriver.png" alt="">
+            <img src="./img/side-devilsriver.png" alt="">
             <small class="card-subtitle">STUDENT LIFE</small>
             <h2 class="title">Devils River Expedition</h2>
             <p class="info">Devils River is a 4-day paddling and camping festival at NOLS University. Past performers include: Sleigh Bells, Matt & Kim, Del the Funky Homosapien, Black Joe Lewis and the Honeybears and more.</p>
           </div>
 
           <div class="card">
-            <img src="static/img/side-shuksan.png" alt="">
+            <img src="./img/side-shuksan.png" alt="">
             <small class="card-subtitle">NEW SKILLS</small>
             <h2 class="title">Mount Shuksan Expedtion</h2>
             <p class="info">Mount Shuksan is a 4-day paddling and camping festival at NOLS University. Past performers include: Sleigh Bells, Matt & Kim, Del the Funky Homosapien, Black Joe Lewis and the Honeybears and more.</p>
@@ -89,19 +89,13 @@
     <!-- white background that's animated -->
     <div class="nav-content"></div>
 
-    <div class="content" style="background-image: url('static/img/bugaboo-spire.jpg')">
+    <div class="content">
       <!-- homepage content -->
       <div class="tagline">
         <h1>Foster the Next Generation. Give Today to Reach Tomorrow.</h1>
         <button class="button-donate">DONATE TO NOLS</button>
       </div>
     </div>
-
-    <a class="github-icon" href="https://github.com/PaddyMurphy/handsome" title="view on github">
-      <b class="show-accessible">view on github</b>
-      <svg aria-hidden="true" class="octicon octicon-mark-github" height="24" version="1.1" viewBox="0 0 16 16" width="24"><path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z" /></svg>
-    </a>
-
   </div>
 </template>
 
